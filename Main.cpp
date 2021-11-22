d631c120bdd73a503fa50838572820d994049017
Benjamin Milhet <66473784+Orchanyne@users.noreply.github.com>
2021-11-22 16:10:55 +0100

    gitignore + test read and write

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,8 @@ int main(int argc, char const *argv[])
 {
     enigma::Enigma e;
     e.createKey();
-    e.read("test.txt");
+    e.read("../test.txt");
+    //e.read("test.txt");
 
     std::cout << e.getKeyToString() << std::endl;
 
